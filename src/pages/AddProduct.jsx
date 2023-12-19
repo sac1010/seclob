84ac84f6130b581b972de2336bb455d9bd9b0bed
sac1010 <sachingirish101@gmail.com>
2023-12-19 23:53:04 +0530

    add product feature added for admin

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,16 +1,112 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AddProduct = () => {
+  const [data, setData] = useState([]);
+  const [category, setCategory] = useState("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const getData = async () => {
+    const data = await axios.get(
+      "https://seclob-server.onrender.com/api/categories"
+    );
+    setData(data.data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const addItem = async () => {
+    console.log(category, "cat");
+    if (!name || !price || !quantity || !category) {
+      alert("please fill required fields");
+      return;
+    }
+    try {
+      let res = await axios.post(
+        `https://seclob-server.onrender.com/api/categories/${category}/products`,
+        { name, image: imageUrl }
+      );
+      console.log(res.data);
+      setCategory("");
+      setImageUrl("");
+      setName("");
+      setPrice("");
+      setQuantity("");
+      alert("product added");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="flex flex-col gap-8">
-        <input type="text" className="border border-gray-600 rounded px-2" placeholder="product name" name="" id="" />
-        <select className="border border-gray-600 rounded px-2" name="category" id="">
+        <input
+        value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          className="border border-gray-600 rounded px-2"
+          placeholder="product name"
+          name=""
+          id=""
+        />
+        <input
+        value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+          type="text"
+          className="border border-gray-600 rounded px-2"
+          placeholder="imageUrl"
+          name=""
+          id=""
+        />
+
+        <select
+        value={category}
+          onChange={(e) =>{
+            console.log(e.target.value)
+            setCategory(e.target.value)
+          }
+          }
+          className="border border-gray-600 rounded px-2"
+          name="category"
+          id=""
+        >
           <option value="">choose category</option>
+          {data.map((el) => {
+            return (
+              <option key={el.id} value={el._id}>
+                {el.category}
+              </option>
+            );
+          })}
         </select>
-        <input className="border border-gray-600 rounded px-2" type="number" placeholder="Price" name="" id="" />
-        <input className="border border-gray-600 rounded px-2" type="number" name="" id="" placeholder="quantity" />
-        <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">add Product</button>
+        <input
+        value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          className="border border-gray-600 rounded px-2"
+          type="number"
+          placeholder="Price"
+          name=""
+          id=""
+        />
+        <input
+        value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          className="border border-gray-600 rounded px-2"
+          type="number"
+          name=""
+          id=""
+          placeholder="quantity"
+        />
+        <button
+          onClick={addItem}
+          className="bg-blue-600 text-white px-4 py-2 rounded-lg"
+        >
+          add Product
+        </button>
       </div>
     </div>
   );
--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -5,28 +5,29 @@ const AdminPanel = () => {
   return (
     <div className="w-full flex justify-around mt-10 flex-col items-center gap-10">
       <Link
-        to={"/admin/remove-category"}
-        className="bg-blue-600 text-white px-4 py-2 rounded-lg"
-      >
-        View Products
-      </Link>
-      <Link
         to={"/admin/add-product"}
         className="bg-blue-600 text-white px-4 py-2 rounded-lg w-fit "
       >
         add product
       </Link>
       <Link
-        to={"/admin/remove-product"}
-        className="bg-blue-600 text-white px-4 py-2 rounded-lg"
-      >
-        Remove Product
-      </Link>
-      <Link
         to={"/admin/add-category"}
         className="bg-blue-600 text-white px-4 py-2 rounded-lg"
       >
         add category
+      </Link>
+      <Link
+        to={"/admin/remove-category"}
+        className="bg-blue-600 text-white px-4 py-2 rounded-lg"
+      >
+        View Products
+      </Link>
+
+      <Link
+        to={"/admin/remove-product"}
+        className="bg-blue-600 text-white px-4 py-2 rounded-lg"
+      >
+        Remove Product
       </Link>
       <Link
         to={"/admin/remove-category"}
