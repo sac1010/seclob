86ce869f8d6e1aa5968fe73c476817cb42ab95b7
sac1010 <sachingirish101@gmail.com>
2023-12-19 12:17:02 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-[#2C65DA] bg-no-repeat bg-center w-full h-[650px]">
+ 
+    </div>
+  );
+};
+
+export default Header;
